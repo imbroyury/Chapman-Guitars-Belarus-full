6ef55c6ccc0e439308d6046a3a9246df51a37aeb
Yury Imbro <imbroyury@gmail.com>
2020-02-02 19:25:14 +0300

    remove client folder

--- client/js/artist-page-toggle-description.js
+++
@@ -1,7 +0,0 @@
-let main = document.querySelector('.main-section');
-
-main.addEventListener('click', function(event) {
-  if (event.target.tagName === 'H2' || event.target.classList.contains('artist-photo')) {
-    event.target.closest('.artist-section').querySelector('.artist-description').classList.toggle('visible');
-  }
-});
