32e79130c0857e55df50357d381a80e9db139e47
Yury Imbro <imbroyury@gmail.com>
2020-02-23 16:09:03 +0300

    routes extracted

---
+++ admin-interface/src/routes.js
@@ -0,0 +1,66 @@
+import GalleryImages from './views/GalleryImages/GalleryImages';
+import AddGalleryImage from './views/GalleryImages/AddGalleryImage';
+import Artists from './views/Artists/Artists';
+import AddArtist from './views/Artists/AddArtist';
+import Login from './views/Login';
+import GuitarSeries from './views/GuitarSeries/GuitarSeries';
+import AddGuitarSeries from './views/GuitarSeries/AddGuitarSeries';
+import Guitars from './views/Guitars/Guitars';
+import AddGuitar from './views/Guitars/AddGuitar';
+import AddGuitarColor from './views/GuitarColor/AddGuitarColor';
+
+export const routes = {
+  preAuth: [
+    [{
+      View: Login,
+      path: '/login',
+      linkLabel: 'Log in',
+    }],
+  ],
+  auth: [
+    [{
+      View: GalleryImages,
+      path: '/gallery-images',
+      linkLabel: 'Gallery Images',
+    },
+    {
+      View: AddGalleryImage,
+      path: '/add-gallery-image',
+      linkLabel: 'Add Gallery Image',
+    }],
+    [{
+      View: Artists,
+      path: '/artists',
+      linkLabel: 'Artists',
+    },
+    {
+      View: AddArtist,
+      path: '/add-artist',
+      linkLabel: 'Add artist',
+    }],
+    [{
+      View: GuitarSeries,
+      path: '/guitar-series',
+      linkLabel: 'Guitar Series'
+    },
+    {
+      View: AddGuitarSeries,
+      path: '/add-guitar-series',
+      linkLabel: 'Add Guitar Series'
+    }],
+    [{
+      View: Guitars,
+      path: '/guitars',
+      linkLabel: 'Guitars'
+    },
+    {
+      View: AddGuitar,
+      path: '/add-guitar',
+      linkLabel: 'Add Guitar'
+    }],
+    [{
+      View: AddGuitarColor,
+      path: '/add-guitar-color/:guitarId',
+    }]
+  ],
+};
