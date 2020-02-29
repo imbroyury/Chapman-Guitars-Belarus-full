7fd08cf563bec6e6e563d4d832dfb1f2d172eaa7
Yury Imbro <imbroyury@gmail.com>
2020-02-29 20:43:23 +0300

    fixed path to uploads

--- server/services/FSService/index.js
+++ server/services/FSService/index.js
@@ -3,7 +3,7 @@ import util from 'util';
 import path from 'path';
 const unlink = util.promisify(fs.unlink);
 
-const PATH_TO_UPLOADS = path.join(__dirname, '..', '..', 'static', 'uploads');
+const PATH_TO_UPLOADS = path.join(__dirname, '..', '..', '..', 'static', 'uploads');
 
 export const removeFileFromUploads = async (filename) => {
   const pathToFile = path.join(PATH_TO_UPLOADS, filename);
